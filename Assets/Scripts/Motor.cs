5bcdb011314f3b5ce50ea871ea2b2d09d1380c4d
mauriciojuba <mauriciojuba@outlook.com>
2016-11-01 01:01:34 -0200

    First Commit

---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+
+	public float vel;
+	Rigidbody2D rb;
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.AddForce(new Vector2(vel,0));
+		
+		}
+
+	}
+}
