755e551cd8d78ef268b56ad7cc7510db24f3eeb7
mauriciojuba <mauriciojuba@outlook.com>
2016-11-22 14:03:13 -0200

    Sounds

---
+++ Assets/Scripts/Sound/AudioEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "AudioEvent", menuName = "Audio/Audio Event", order = 1)]
+public class AudioEvent : ScriptableObject{
+	public string audioEventName = "null";
+	public AudioClip audioClip;
+
+	[Range(0.0f,1.0f)]
+	public float volume = 1.0f;
+	[Range(-3.0f,3.0f)]
+	public float pitch = 1.0f;
+	public bool Loop;
+}
---
+++ Assets/Scripts/Sound/AudioManagerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(AudioManager))]
+public class AudioManagerEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		AudioManager myTarget = (AudioManager)target;
+		DrawDefaultInspector();
+		EditorGUILayout.LabelField("Volume " + myTarget.getVolume(myTarget.eventName));
+		EditorGUILayout.LabelField("Pitch " + myTarget.getPitch(myTarget.eventName));
+		if(GUILayout.Button("Play Audio")){
+			myTarget.playSound(myTarget.eventName);
+		}
+		if(GUILayout.Button("Stop Audio")){
+			myTarget.stopSound();
+		}
+	}
+
+}
